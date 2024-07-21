6eb1ea7c1cc08ee48b86d82f4d4047ffc39d46f7
iJaabl <jaabel@pm.me>
2024-07-20 23:05:17 -0500

    updated security

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const Schema = mongoose.Schema
+const bcrypt = require("bcryptjs")
 require("dotenv/config")
 
 const port = process.env.PORT || 8020
@@ -26,24 +27,15 @@ const db = mongoose.connection
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 
-const Account = new Schema(
-  {
-    admin: { type: Boolean, default: false },
-    subscription: { type: String, default: "free" },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    username: { type: String, required: true },
-    password: { type: String, min: 6, max: 1024, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-  },
-  { timestamps: true }
-)
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8q6kx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const Account = new Schema({
+  admin: { type: Boolean, default: false },
+  subscription: { type: String, default: "free" },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, min: 6, max: 1024, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 })
 
 // --- MIDDLEWARE ---
@@ -69,27 +61,62 @@ const handleCallback = (callback) => async (req, res, next) => {
 // --- CONTROLLERS ---
 
 const createAccount = handleCallback(async (req, res) => {
-  const account = new Account(req.body)
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+  const account = await new Account({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+  })
+
   await account.save()
   return res.status(201).json({
     success: true,
-    data: account,
+    message: "Account created",
   })
 })
 
-const updateAccount = handleCallback(async ({ body, params }, res) => {
-  console.log("starting update \n")
-  console.log("update obj: \n", body)
+const signin = async (req, res) => {
+  activeAccount = await Account.findOne({ username: req.body.username })
+  !activeAccount && res.status(400).json("Wrong credentials")
 
-  const { id } = params
-  const account = await Account.findByIdAndUpdate(id, body, {
-    new: true,
-    returnDocument: "after",
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    activeAccount.password
+  )
+  !validPassword && res.status(400).json("Wrong credentials")
+
+  activeAccount.password = undefined
+
+  res.status(200).json({ success: true, data: activeAccount })
+}
+
+const verify = async (req, res) => {
+  const verified = await Account.validate({
+    email: req.body.email,
+    password: req.body.password,
   })
+  if (!verified)
+    throw {
+      success: false,
+      message: "Wrong credentials",
+    }
+  return res.status(200).json({ success: true, data: verified })
+}
 
-  if (account) {
-    console.log("success \n", account)
-    return res.status(200).json({ success: true, data: account })
+const updateAccount = handleCallback(async ({ body, params }, res) => {
+  const { id } = params
+
+  if (req.body.id === id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10)
+      body.password = await bcrypt.hash(body.password, salt)
+    }
+    const account = await Account.findByIdAndUpdate(id, { $set: req.body })
+    return res.status(200).json({ success: true, message: "Account updated" })
+  } else {
+    throw new Error("Unauthorized")
   }
 })
 
@@ -117,14 +144,11 @@ const getAccount = handleCallback(async (req, res) => {
 // --- ROUTES ---
 
 router.get("/", (res, req) => res.json("base api endpoint"))
+router.post("/signin", signin)
+router.post("/verify", verify)
 router.get("/account/:id", getAccount)
-router.get("/account/:id/chatflow", getChatflowsByAccount)
-
 router.put("/account/:id", updateAccount)
-router.put("/account/:id/store/", chatStoreInAccount)
-
 router.post("/account/create", createAccount)
-
 router.delete("/account/:id", deleteAccount)
 
 app.listen(port, () => {
