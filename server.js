2f49d78fb146978ed12a39d3a9f553bb5f042180
iJaabl <jaabel@pm.me>
2024-07-20 22:25:56 -0500

    pushed final changes to run a basic backend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
+const { Router } = require("express")
+const router = Router()
 const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const Schema = mongoose.Schema
 require("dotenv/config")
 
 const port = process.env.PORT || 8020
@@ -24,6 +26,26 @@ const db = mongoose.connection
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 
+const Account = new Schema(
+  {
+    admin: { type: Boolean, default: false },
+    subscription: { type: String, default: "free" },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, min: 6, max: 1024, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8q6kx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
 // --- MIDDLEWARE ---
 
 app.use(
@@ -34,113 +56,76 @@ app.use(
 app.use(bodyParser.json())
 app.use(express.json())
 
-// ---
+// --- UTILITIES ---
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8q6kx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-async function run() {
+const handleCallback = (callback) => async (req, res, next) => {
   try {
-    await client.connect()
-    const database = client.db("the_fractions")
-    const fractionsCollection = database.collection("fractions")
-    const usersCollection = database.collection("users")
-
-    app.get("/fractions", async (req, res) => {
-      const cursor = fractionsCollection.find({})
-      const fractions = await cursor.toArray()
-      res.send(fractions)
-    })
-
-    app.get("/fractions/:id", async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) }
-      const fraction = await fractionsCollection.findOne(query)
-      res.send(fraction)
-    })
-
-    app.post("/fractions", async (req, res) => {
-      const fraction = req.body
-      const result = await fractionsCollection.insertOne(fraction)
-      res.json(result)
-    })
-
-    app.put("/fractions/:id", async (req, res) => {
-      const id = req.params.id
-      const filter = { _id: ObjectId(id) }
-      const fraction = req.body
-      const option = { upsert: true }
-      const updateFraction = {
-        $set: {
-          name: fraction.name,
-          description: fraction.description,
-          image: fraction.image,
-          price: fraction.price,
-          quantity: fraction.quantity,
-        },
-      }
-      const result = await fractionsCollection.updateOne(
-        filter,
-        updateFraction,
-        option
-      )
-      res.json(result)
-    })
-
-    app.delete("/fractions/:id", async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) }
-      const result = await fractionsCollection.deleteOne(query)
-      res.json(result)
-    })
-
-    app.post("/users", async (req, res) => {
-      const user = req.body
-      const result = await usersCollection.insertOne(user)
-      res.json(result)
-    })
-
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email
-      const query = { email: email }
-      const user = await usersCollection.findOne(query)
-      let isAdmin = false
-      if (user?.role === "admin") {
-        isAdmin = true
-      }
-      res.json({ admin: isAdmin })
-    })
-
-    app.put("/users", async (req, res) => {
-      const user = req.body
-      const filter = { email: user.email }
-      const options = { upsert: true }
-      const updateDoc = { $set: user }
-      const result = await usersCollection.updateOne(filter, updateDoc, options)
-      res.json(result)
-    })
-
-    app.put("/users/admin", async (req, res) => {
-      const user = req.body
-      const filter = { email: user.email }
-      const updateDoc = { $set: { role: "admin" } }
-      const result = await usersCollection.updateOne(filter, updateDoc)
-      res.json(result)
-    })
-  } finally {
-    // await client.close();
+    await callback(req, res, next)
+  } catch (error) {
+    next(error)
   }
 }
 
-run().catch(console.dir)
+// --- CONTROLLERS ---
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+const createAccount = handleCallback(async (req, res) => {
+  const account = new Account(req.body)
+  await account.save()
+  return res.status(201).json({
+    success: true,
+    data: account,
+  })
 })
+
+const updateAccount = handleCallback(async ({ body, params }, res) => {
+  console.log("starting update \n")
+  console.log("update obj: \n", body)
+
+  const { id } = params
+  const account = await Account.findByIdAndUpdate(id, body, {
+    new: true,
+    returnDocument: "after",
+  })
+
+  if (account) {
+    console.log("success \n", account)
+    return res.status(200).json({ success: true, data: account })
+  }
+})
+
+const deleteAccount = handleCallback(async (req, res) => {
+  const { id } = req.params
+  const deleted = await Account.findByIdAndDelete(id)
+  if (deleted) {
+    return res
+      .status(200)
+      .json({ success: true, data: deleted, Message: "Account deleted" })
+  }
+  throw new Error("Account not found")
+})
+
+const getAccount = handleCallback(async (req, res) => {
+  console.log(req.params)
+  const { id } = req.params
+  const account = await Account.findById(id)
+  if (account) {
+    return res.status(200).json({ success: true, data: account })
+  }
+  throw new Error("Account not found")
+})
+
+// --- ROUTES ---
+
+router.get("/", (res, req) => res.json("base api endpoint"))
+router.get("/account/:id", getAccount)
+router.get("/account/:id/chatflow", getChatflowsByAccount)
+
+router.put("/account/:id", updateAccount)
+router.put("/account/:id/store/", chatStoreInAccount)
+
+router.post("/account/create", createAccount)
+
+router.delete("/account/:id", deleteAccount)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
